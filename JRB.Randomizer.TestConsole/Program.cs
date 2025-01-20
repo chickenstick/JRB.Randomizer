d37de5b033fbfb5627d3eb60594e4582603af2ca
chickenstick <joseph_bohr@hotmail.com>
2025-01-20 14:30:04 -0600

    Randomizer is now a collection of extension methods.

--- JRB.Randomizer.TestConsole/Program.cs
+++ JRB.Randomizer.TestConsole/Program.cs
@@ -11,11 +11,14 @@ namespace JRB.Randomizer.TestConsole
 
         static void Main(string[] args)
         {
-            TestRandomizationSpread();
+            //TestRandomizationSpread();
+            TestRandomizedOrder();
         }
 
         static void TestRandomizationSpread()
         {
+            Random random = Random.Shared;
+
             Dictionary<char, int> results = new Dictionary<char, int>
             {
                 { 'A', 0 },
@@ -28,7 +31,7 @@ namespace JRB.Randomizer.TestConsole
             Stopwatch sw = Stopwatch.StartNew();
             for (int i = 0; i < 1000000; i++)
             {
-                char value = Randomizer.GetRandom('A', 'B', 'C', 'D');
+                char value = random.GetRandom('A', 'B', 'C', 'D');
                 if (results.ContainsKey(value))
                 {
                     results[value]++;
@@ -44,6 +47,92 @@ namespace JRB.Randomizer.TestConsole
             foreach (var item in results)
             {
                 Console.WriteLine($"{item.Key}:  {item.Value}");
+            }
+        }
+
+        static void TestRandomizedOrder()
+        {
+            Random random = Random.Shared;
+
+            char[] allKeys = new char[] { 'A', 'B', 'C', 'D', 'E' };
+            Dictionary<char, int[]> results = new Dictionary<char, int[]>();
+            foreach (char key in allKeys)
+            {
+                int[] array = new int[allKeys.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = 0;
+                }
+                results.Add(key, array);
+            }
+
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < 1000000; i++)
+            {
+                char[] randomizedList = random.Randomize(allKeys).ToArray();
+
+                if (!OriginalUnchanged())
+                {
+                    return;
+                }
+
+                for (int index = 0;  index < randomizedList.Length; index++)
+                {
+                    char key = randomizedList[index];
+                    results[key][index] += 1;
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Elapsed time:  {sw.Elapsed}");
+            Console.WriteLine("\t" + string.Join("\t", Enumerable.Range(1, allKeys.Length).Select(i => $"{i}:")));
+            foreach (KeyValuePair<char, int[]> item in results)
+            {
+                Console.Write($"{item.Key}:\t");
+                for (int index = 0; index < item.Value.Length; index++)
+                {
+                    Console.Write($"{item.Value[index]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            bool OriginalUnchanged()
+            {
+                if (allKeys[0] != 'A')
+                {
+                    WriteError($"First item should have been 'A', but was '{allKeys[0]}'.");
+                    return false;
+                }
+                else if (allKeys[1] != 'B')
+                {
+                    WriteError($"Second item should have been 'B', but was '{allKeys[1]}'.");
+                    return false;
+                }
+                else if (allKeys[2] != 'C')
+                {
+                    WriteError($"Third item should have been 'C', but was '{allKeys[2]}'.");
+                    return false;
+                }
+                else if (allKeys[3] != 'D')
+                {
+                    WriteError($"Fourth item should have been 'D', but was '{allKeys[3]}'.");
+                    return false;
+                }
+                else if (allKeys[4] != 'E')
+                {
+                    WriteError($"Fifth item should have been 'E', but was '{allKeys[4]}'.");
+                    return false;
+                }
+
+                return true;
+            }
+
+            void WriteError(string message)
+            {
+                ConsoleColor original = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ForegroundColor = original;
             }
         }
 
---
+++ JRB.Randomizer/RandomExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRB.Randomizer
+{
+    public static class RandomExtensions
+    {
+
+        public static IEnumerable<T> Randomize<T>(this Random random, IEnumerable<T> original)
+        {
+            T[] array = [.. original];
+            random.Shuffle(array);
+            foreach (T item in array)
+                yield return item;
+        }
+
+        public static T GetRandomExcludingOne<T>(this Random random, IEnumerable<T> list, Func<T, bool> testEquality)
+        {
+            IList<T> excluded = list.Where(x => !testEquality(x)).ToList();
+            return GetRandom(random, excluded);
+        }
+
+        public static T GetRandom<T>(this Random random, IList<T> list)
+        {
+            int index = random.Next(0, list.Count);
+            return list[index];
+        }
+
+        public static T GetRandom<T>(this Random random, params T[] items)
+        {
+            return GetRandom(random, items as IList<T>);
+        }
+
+        public static T GetRandom<T>(this Random random, params Func<T>[] methods)
+        {
+            int index = random.Next(0, methods.Length);
+            return methods[index]();
+        }
+
+    }
+}
